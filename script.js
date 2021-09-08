81d405fd52fa281a6cbee61900dd782243f23325
LukPokemon <90333874+LukPokemon@users.noreply.github.com>
2021-09-08 17:16:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let signInButton = document.getElementById("signIn");
+
+signInButton.onclick = function(){
+    let userName = window.prompt("Username:");
+    let password = window.prompt("Password:");
+
+    if (userName == "LukCoding") {
+        if (password == "Work_mindthing667$") {
+            alert("Welcome back, LukCoding!");
+        }
+    }
+
+    if (userName == "AstroMiastro") {
+        if (password == "UnknownEstroDoughnut56_") {
+            alert("Welcome back, AstroMiastro!");
+        }
+    }
+    if (userName == "LukassBiggestFunEver") {
+        if (password == "HurrayWebsite8800!") {
+            alert("Welcome back, LukassBiggestFunEver!");
+        }
+    }
+
+    if (userName == "||_xNyx_||") {
+        if (password == "|mikolajChatsonDiscord1234|") {
+            alert("Welcome back, ||_xNyx_||!");
+        }
+    }
